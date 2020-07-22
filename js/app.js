36d0a522f9d169fec1243390cc6262899d944542
ijeomaemeruwa <helloijeoma@gmail.com>
2020-07-22 22:38:40 +0100

    added transition

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ async function homePage(getMovie) {
   header.innerHTML = `<h2 class="header-text">UPCOMING MOVIES...</h2>`;
 
   movieShowcase.innerHTML = data.results.map(movie => `
-     <div class="movie-results thumbnail">
+     <div class="movie-results">
 
      <img src= "https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}" alt= "${movie.title}" class="img-responsive"/>
 
-     <div class="movie-info" data-movieID= "${movie.id}">
+     <div class="movie-info" data-movieId= "${movie.id}">
      <h6>${movie.title}</h6>
      </div>
      </div>
@@ -21,12 +21,30 @@ async function homePage(getMovie) {
   .join('');
 }
 
-//Get Movie ID and display on the DOM
-async function getMovieById(movieID) {
+//Get Movie details
+movieShowcase.addEventListener('click', (e) => {
+  const movieInfo = e.path.find(item => {
+    if(item.classList) {
+      return item.classList.contains("movie-info");
+    } else {
+      return false;
+    }
+  });
+  
+  if(movieInfo) {
+    const movieData = movieInfo.getAttribute('data-movieId');
+
+    getMovieById(movieData);
+  }
+});
+
+
+//Get Movie ID from API and display on the DOM
+async function getMovieById(movieData) {
 
   movieShowcase.innerHTML = '';
 
-  const res = await fetch(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${key}`);
+  const res = await fetch(`https://api.themoviedb.org/3/{movie_id}movie/?api_key=${key}&query=${movieData}`);
   const dataResponse = await res.json();
   console.log(dataResponse);
 
@@ -36,7 +54,7 @@ async function getMovieById(movieID) {
   addInfoToDOM();
 }
 
-// Add movie info to DOM
+// Add movie details to the DOM
 function addInfoToDOM() {
    infoSection.innerHTML = data.results.map(movie => `
      <div class="info-section>
@@ -54,36 +72,12 @@ function addInfoToDOM() {
    );
 }
 
-//Get new set of upcoming movies on page reload
-async function getUpcomingMovies() {
-  const randomRes = await fetch(``);
-  const randomData = await randomRes.json();
-  console.log(randomData);
-
-  homePage();
-}
 
 
-//Display movie details
-movieShowcase.addEventListener('click', (e) => {
-  const movieInfo = e.path.find(item => {
-    if(item.classList) {
-      return item.classList.contains("movie-info");
-    } else {
-      return false;
-    }
-  });
-  if(movieInfo) {
-    const movieID = movieInfo.querySelector('data-movieID');
+//Add Event Listeners To home search button
 
-    getMovieById(movieID);
-  }
-});
-
-
-//Add Event Listeners
-refreshPage.addEventListener('click', getUpcomingMovies);
 home.addEventListener('click', homePage);
+
 
 
 
--- js/movie.js
+++ js/movie.js
@@ -3,10 +3,9 @@ const key = '8f41bdefbf350320309f2cb50de6e68b';
     
 
 // Select Dom Elements
-const refreshPage = document.querySelector('#refresh-button');
 const home = document.querySelector('#home-button');
-const infoSection = document.querySelector('#movie-details'); 
 const header = document.querySelector('#movie-header'); 
 const movieShowcase = document.querySelector('#movies');
+const infoSection = document.querySelector('#movie-details'); 
 
 
